6c0f926c7893440aae45c534a44326e28c5e2218
Kota <maedakoutaa@gmail.com>
2022-04-06 18:23:44 +0900

    Initial Commit

---
+++ MiniApp36-Alamofire01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp36-Alamofire01
+//
+//  Created by 前田航汰 on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
